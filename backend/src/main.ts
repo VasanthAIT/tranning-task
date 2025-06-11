02db1db3bf7f0f1527985f5c3f9fb188eb023863
Vasanth <vasanth12ait@gmail.com>
2025-06-11 13:23:51 +0530

    4 commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,14 +6,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+ 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
+  app.enableCors({
+  origin: 'http://localhost:3001',
+});
 
   app.useGlobalPipes(new ValidationPipe());
-  app.useBodyParser('json');
-  app.enableCors();
+  
+ 
   await app.listen(3000);
   console.log(`Backend server running at http://localhost:3000`);
 }
